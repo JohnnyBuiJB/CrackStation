49a6cb5ee37cc5b5bc529d2828debb88f925d57e
Vy Bui <buivy@oregonstate.edu>
2022-10-29 13:56:51 -0700

    [POC_v1] implement

--- CrackStation/Package.swift
+++ CrackStation/Package.swift
@@ -26,9 +26,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CrackStation",
-            dependencies: []),
+            resources: [
+                .process("HashDict.json")
+            ]
+        ),
         .testTarget(
             name: "CrackStationTests",
-            dependencies: ["CrackStation"]),
+            dependencies: ["CrackStation"]
+        ),
     ]
 )
--- CrackStation/Sources/CrackStation/CrackStation.swift
+++ CrackStation/Sources/CrackStation/CrackStation.swift
@@ -1,11 +1,31 @@
+import Foundation
 
 class CrackStation: Decrypter {
+    var lookupTable: [String: String] = [:]
+
+    static func loadDictionaryFromDisk() throws -> [String : String] {
+        guard let path = Bundle.module.url(forResource: "HashDict", withExtension: "json") else { return [:] }
+
+        let data = try Data(contentsOf: path)
+        let jsonResult = try JSONSerialization.jsonObject(with: data)
+
+        if let lookupTable: Dictionary = jsonResult as? Dictionary<String, String> {
+            return lookupTable
+        } else {
+            return [:]
+        }
+    }
+    
     required init() {
-        // TODO: Implement
+        do {
+            lookupTable = try CrackStation.loadDictionaryFromDisk()
+        }
+        catch {
+            print("Initialization fails!")
+        }
     }
 
     func decrypt(shaHash: String) -> String? {
-        // TODO: Implement
-        return nil
+        return lookupTable[shaHash]
     }
 }
