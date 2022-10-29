ab0f08c7fda3ae4cd3342ab0ae9186610d9d49e1
Vy Bui <buivy@oregonstate.edu>
2022-10-29 14:37:16 -0700

    [POC_v2] add hash generator and update lookup table

---
+++ CrackStation/Sources/CrackStation/hash_dict_generator.py
@@ -0,0 +1,23 @@
+import hashlib
+import json
+from string import ascii_lowercase, ascii_uppercase
+import os
+
+# Serializing json
+# json_object = json.dumps(dictionary, indent=4)
+
+def main():
+    cur_dir = os.getcwd()
+    
+    dict = {}
+    alphabet = ascii_lowercase + ascii_uppercase + "0123456789"
+    
+    for c1 in alphabet:
+        for c2 in alphabet:
+            dict[hashlib.sha1((c1 + c2).encode('utf-8')).hexdigest()] = (c1 + c2)
+    
+    with open(cur_dir + "/HashDict.json", "w") as file:
+        file.write(json.dumps(dict, indent=4))
+
+if __name__ == "__main__":
+    main()
